988da31eaf4f182284135e7cf29403c057ac96f4
sreeja99 <srijagodishala@gmail.com>
2020-09-24 21:54:36 +0530

    Added:Rolling the die to get a number

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -5,6 +5,9 @@ public class secDay {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int position = 0;
+		double dieroll = (Math.floor(Math.random() *10 )%6)+1;
+		System.out.println("dieroll:"+dieroll);
+		position += dieroll;
 		System.out.println("current position:"+position);
 
 	}

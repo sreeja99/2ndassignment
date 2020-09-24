c1b0ac3a8b07dc9161c6c3db5fb4fdcaec45d051
sreeja99 <srijagodishala@gmail.com>
2020-09-24 21:51:09 +0530

    Added:Assigned single player position to zero

---
+++ SnakeAndLadder.java
@@ -0,0 +1,12 @@
+package game;
+
+public class secDay {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int position = 0;
+		System.out.println("current position:"+position);
+
+	}
+
+}

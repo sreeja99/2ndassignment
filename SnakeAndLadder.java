4980cc841145390d1cf67d203cb6b6f74e73e39d
sreeja99 <srijagodishala@gmail.com>
2020-09-25 10:11:32 +0530

    Added:Repeat Till Player Reaches Winning Position 100

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,31 +1,39 @@
 package game;
 
-public class secDay {
-	public static  final int NO_PLAY = 0;
-	public static  final int LADDER = 1;
-	public static  final int SNAKE =2;
-      public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int position = 0;
-		double dieroll = (Math.floor(Math.random() *10 )%6)+1;
-		System.out.println("dieroll:"+dieroll);
-		double option = (Math.floor(Math.random() *10 )%3);
-		System.out.println("option:" +option);
-		switch((int)option) {
-		case NO_PLAY :
-			System.out.println("position:"+(position += position));
-			break;
-		case LADDER :
-			System.out.println("position:"+(position += dieroll));
-			break;
-		case SNAKE :
-			System.out.println("position:"+(position -= dieroll));
-			break;
-			
-		}
-		
+public class ucSeven {
+	public static final int NO_PLAY=0;
+	public static final int LADDER=1;
+	public static final int SNAKE=2;
 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Variables
+		int position=0;
+
+		//Welcome statement
+		System.out.println("Welcome to Snake and Ladder Simulator");
+
+		while(position<100) {
+			//computation
+			int dieroll=(int) (Math.floor(Math.random()*10)%7);
+			int option=(int) (Math.floor(Math.random()*10)%3);
+
+			switch(option) {
+			case NO_PLAY:
+				position=position;
+				break;
+			case LADDER :
+				position+=dieroll;
+				break;
+			case SNAKE:
+				if (position-dieroll>=0)
+					position-=dieroll;
+				else
+				position=0;
+				break;
+			}
+			System.out.println(position);
+		}
 	}
 
 }
-

f9ba0b99c9b5e64b834305740d387d3656e97668
sreeja99 <srijagodishala@gmail.com>
2020-09-25 10:32:10 +0530

    Added:Report Total Turns Till Winning and Player Position

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -9,33 +9,43 @@ public class ucSeven {
 		// TODO Auto-generated method stub
 		//Variables
 		int position=0;
+		int count =0;
 
 		//Welcome statement
 		System.out.println("Welcome to Snake and Ladder Simulator");
 
 		while(position<100) {
 			//computation
+			count+=1;
 			int dieroll=(int) (Math.floor(Math.random()*10)%7);
 			int option=(int) (Math.floor(Math.random()*10)%3);
 
 			switch(option) {
 			case NO_PLAY:
 				position=position;
+				System.out.println("position:"+position);
 				break;
 			case LADDER:
-				if (position+dieroll>100)
+				if (position+dieroll>100) {
 					position=position;
+				    System.out.println("position:"+position);
+				}
 				else
 					position+=dieroll;
+				System.out.println("position:"+position);
 				break;
 			case SNAKE:
-				if (position-dieroll>=0)
+				if (position-dieroll>=0) {
 					position-=dieroll;
+				    System.out.println("position:"+position);
+				}
 				else
 					position=0;
+				System.out.println("position:"+position);
 				break;
 			}
-			System.out.println(position);
+			System.out.println("Turns:"+count);
+			System.out.println("position:"+position);
 		}
 	}
 
